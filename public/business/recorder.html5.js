511de50c5ef90541f792a21ee62c5e06700cdb70
ra61 <914466326@qq.com>
2018-06-25 15:28:38 +0800

    bootstrap

--- public/business/recorder.html5.js
+++ public/business/recorder.html5.js
@@ -3,49 +3,41 @@ if (!Recorder.isRecordingSupported()) {
 } else {
 
     var recorder;
+    var flag;
 
     init.addEventListener("click", function () {
         init.disabled = true;
-        start.disabled = false;
-        monitorGain.disabled = true;
-        recordingGain.disabled = true;
+        reset.disabled = false;
+        control.disabled = false;
+   
         numberOfChannels.disabled = true;
         bitDepth.disabled = true;
         format.disabled = true;
 
         recorder = ns.createRecorder[format.value]();
 
-        pause.addEventListener("click", function () { recorder.pause(); });
-        resume.addEventListener("click", function () { recorder.resume(); });
-        stopButton.addEventListener("click", function () { recorder.stop(); });
-        start.addEventListener("click", function () {
-            recorder.start().catch(function (e) {
-                ns.screenLogger('Error encountered:', e.message);
-            });
-        });
+        flag = 'start';
+
+        control.onclick = function () {
+            if (flag === 'start') {
+                recorder.start().catch(function (e) {
+                    ns.screenLogger('Error encountered:', e.message);
+                });
+            } else {
+                recorder.stop();
+            }
+        };
 
         recorder.onstart = function () {
             ns.screenLogger('Recorder is started');
-            start.disabled = resume.disabled = true;
-            pause.disabled = stopButton.disabled = false;
+            flag = 'stop';
+            control.innerHTML = '结束录音';
         };
 
         recorder.onstop = function () {
             ns.screenLogger('Recorder is stopped');
-            start.disabled = false;
-            pause.disabled = resume.disabled = stopButton.disabled = true;
-        };
-
-        recorder.onpause = function () {
-            ns.screenLogger('Recorder is paused');
-            pause.disabled = start.disabled = true;
-            resume.disabled = stopButton.disabled = false;
-        };
-
-        recorder.onresume = function () {
-            ns.screenLogger('Recorder is resuming');
-            start.disabled = resume.disabled = true;
-            pause.disabled = stopButton.disabled = false;
+            flag = 'start';
+            control.innerHTML = '开始录音';
         };
 
         recorder.onstreamerror = function (e) {
@@ -58,13 +50,11 @@ if (!Recorder.isRecordingSupported()) {
     });
 
     reset.addEventListener("click", function () {
-        monitorGain.disabled = false;
-        recordingGain.disabled = false;
         numberOfChannels.disabled = false;
         bitDepth.disabled = false;
         format.disabled = false;
         init.disabled = false;
-        start.disabled = true;
-        recorder = undefined;
+        reset.disabled = true;
+        control.disabled = true;
     });
 }
