662c2ad93d4d62e19458948bbc560940c8abdf50
ra61 <6059mowen>
2018-12-10 16:32:32 +0800

    multer

--- server.js
+++ server.js
@@ -21,11 +21,20 @@ const onDeleteFile = require("./modules/onDeleteFile");
 const express = require("express"),
     path = require("path"),
     app = express(),
-
+	multer = require("multer"),
+	upload = multer({ dest: 'uploadedFiles/' }),
     // paths/constants
     publicDir = process.env.PUBLIC_DIR || path.join(__dirname, 'public'),
     nodeModulesDir = process.env.NODE_MODULES_DIR || path.join(__dirname, 'node_modules'),
     port = process.env.SERVER_PORT || 3000;
+	
+//设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    next();
+});
 
 app.listen(port, function () {
     console.log('Listening on port:' + port);
@@ -36,3 +45,21 @@ app.use(express.static(publicDir));
 app.use("/node_modules", express.static(nodeModulesDir));
 app.post("/uploads", onUpload);
 app.delete("/uploads/:uuid", onDeleteFile);
+
+app.post('/onAudioUpload', upload.single('file'), function (req, res, next) {
+
+    console.log(req.file);
+    if (req.file === undefined) {
+        res.json({
+            "code": 0,
+            "message": "请把file name设置为file",
+            "data": null
+        });
+    } else {
+        res.json({
+            "code": 1,
+            "message": "success",
+            "data": req.file
+        })
+    }
+});
